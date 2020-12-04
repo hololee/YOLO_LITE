6778fda8865f16fe4e0bf992af7b912ae27ba1de
hololee <lccandol@naver.com>
2020-12-04 11:52:34 +0900

    First trial on calculate loss logic.

--- run/train.py
+++ run/train.py
@@ -3,6 +3,7 @@ from data.DataManager import get_data_loader
 from models import config as cfg
 from models.YololiteNet import yoloLite as YL
 from utils.yolo_utils import calculate_loss
+import matplotlib.pyplot as plt
 
 # Load data_loader.
 train_loader, dev_loader, test_loader = get_data_loader()
@@ -17,6 +18,9 @@ device = torch.device(cfg.training_gpu if torch.cuda.is_available() else 'cpu')
 # Set optimizer.
 params = [p for p in yoloLite.parameters() if p.requires_grad]
 optimizer = torch.optim.Adam(params, lr=cfg.learning_rate)
+
+# for keep training loss.
+train_loss = []
 
 
 # One step for training.
@@ -40,7 +44,7 @@ def train_step(gpu, model, input, target, optimizer):
     ############################################## for different size input, not yolo case.##########################################
 
     # forward-prop
-    output = model(torch.stack(img))
+    output = model(torch.stack(input))
 
     # calculate loss.
     total_loss = calculate_loss(gpu, output, target)
@@ -49,8 +53,20 @@ def train_step(gpu, model, input, target, optimizer):
     total_loss.backward()
     optimizer.step()
 
+    # keep and print train loss.
+    train_loss.append(total_loss.detach().cpu().item())
+    print(train_loss[-1])
+
 
 # NOTICE: TRAINING...
 for epoch in range(cfg.total_epoch):
+    print(f'EPOCH : {epoch + 1} / {cfg.total_epoch}')
+
     for iteration, (img, target) in enumerate(train_loader):
+        print(f'iteration : {iteration + 1} ', end='')
+
+        # train one step.
         train_step(device, yoloLite, img, target, optimizer)
+
+    # plot train loss.
+    plt.plot([i for i in range(len(train_loss))], train_loss)
