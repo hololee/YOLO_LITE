4082a565fc0b739af1495d1c63f332c742a3eb63
hololee <lccandol@naver.com>
2020-12-04 11:04:56 +0900

    First trial on calculate loss logic.

--- utils/yolo_utils.py
+++ utils/yolo_utils.py
@@ -50,8 +50,9 @@ def calculate_loss(gpu, output, target):
 
     # Rotate batch images.
     for idx, one_output in enumerate(output):
+        # one output :  # (C, H, W)
         one_target = target[idx]
-        boxes, classes = one_target['boxes'], one_target['classes']
+        boxes, classes = one_target['boxes'], one_target['classes']  # (n_box, 6), (n_box, 1)
         # Rotate G.T bounding boxes.
 
         # all indices of cells.
@@ -64,23 +65,44 @@ def calculate_loss(gpu, output, target):
 
             # remove from all_cell_indices for calculate on not responsible on non-objects.
             all_cell_indices.remove(index_cell)
-            one_box_target = boxes[jdx][2:]  # (4)
-            one_class_target = classes[jdx]
-            class_prob = torch.zeros([cfg.n_classes])
-            class_prob[one_class_target.data] = 1
+
+            # prepare target information.
+            one_box_target = boxes[jdx][2:]  # (4,)
+            one_class_target_raw = classes[jdx]  # (1,)
+            one_class_target = torch.zeros([cfg.n_classes])  # (C,)
+            one_class_target[one_class_target_raw.data] = 1  # one-hot encoding.
 
             # move to gpu.
             one_box_target.to(gpu)
 
-            # NOTICE: Calculate Bounding Box loss.
+            # calculate on multiple bounding box prediction.
+            for n_box in range(cfg.n_boxes):
+                # NOTICE: Calculate Bounding Box loss.
 
-            # NOTICE: Calculate object confidence loss.
+                # center_x
+                loss_bounding_box += torch.square(torch.sub(one_box_target[0], one_output[(5 * n_box), index_cell[1], index_cell[0]]))
+
+                # center_y
+                loss_bounding_box += torch.square(torch.sub(one_box_target[1], one_output[(5 * n_box) + 1, index_cell[1], index_cell[0]]))
+
+                # width
+                loss_bounding_box += torch.square(torch.sub(torch.sqrt(one_box_target[2]), torch.sqrt(one_output[(5 * n_box) + 2, index_cell[1], index_cell[0]])))
+
+                # height
+                loss_bounding_box += torch.square(torch.sub(torch.sqrt(one_box_target[3]), torch.sqrt(one_output[(5 * n_box) + 3, index_cell[1], index_cell[0]])))
+
+                # NOTICE: Calculate object confidence loss.
+                loss_object_conf += torch.square(torch.sub(1, one_output[(5 * n_box) + 4, index_cell[1], index_cell[0]]))  # target is 1.
 
             # NOTICE: Calculate Class Probability loss.
+            loss_class_prob += torch.square(torch.sub(one_class_target, one_output[(5 * cfg.n_boxes):, index_cell[1], index_cell[0]]))
 
-        # NOTICE: calculate object confidence for non-object.
-        for remain_non_object_indices in all_cell_indices:
-            # one_output[:, remain_non_object_indices[1], remain_non_object_indices[0]]
-            pass
+        # calculate on multiple bounding box prediction.
+        for n_box in range(cfg.n_boxes):
+            # for non-object cells.
+            for remain_non_object_indices in all_cell_indices:
+                # NOTICE: calculate object confidence loss for non-object.
+                loss_noon_object_conf += torch.square(torch.sub(0, one_output[(5 * n_box) + 4, remain_non_object_indices[1], remain_non_object_indices[0]]))  # target is 0.
+
     total_loss = (cfg.lambda_coord * loss_bounding_box) + loss_object_conf + (cfg.lambda_noobj * loss_noon_object_conf) + loss_class_prob
     return total_loss
